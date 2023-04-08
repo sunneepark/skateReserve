ad5aab21a9380be56bcb664dd8d09496b86999e4
sunny.pk <sunny.pk@navercorp.com>
2023-04-08 15:46:42 +0900

    feat : layout + scroll
    scroll view 추가

--- App.web.js
+++ App.web.js
@@ -1,12 +1,83 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native'
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const windowHeight = Dimensions.get('window').height;
+console.log(windowHeight)
 
 function App() {
   return (
-    <View>
-	    <Text> first react-native </Text>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView horizontal contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+  //flexDirection: 'row', //모든 view는 flex direction, default는 row 가 아니라 column
+  backgroundColor: 'orange'
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    //flex: 3, scrollview 는 스크린보다 크기 때문에
+    backgroundColor: "blue"
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+})
 export default App
